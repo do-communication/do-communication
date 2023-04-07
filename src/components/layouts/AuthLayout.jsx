4fa11be7697353a77de63d0d58d5d6ecf6fe227b
Senayt Gobezie <senaitgobezie@gmail.com>
2023-04-07 22:51:23 +0300

    progress

---
+++ src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({children}) => {
+    return (<h1 class="text-red-600">{children}</h1>);
+}
+ 
+export default AuthLayout;
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Home = ({ name }) => {
+  return (<main className="flex flex-col items-center w-screen h-screen pt-20">
+  <h1 class="text-2xl">Digital Office Communication</h1>
+  <h1 className="text-lg">Landing Page Here</h1>
+  <ul className="list-disc">
+    <li><Link href="/auth/login" className="text-blue-500 hover:text-blue-400 hover:underline">Login</Link></li>
+    <li><Link href="/auth/signup" className="text-blue-500 hover:text-blue-400 hover:underline">Signup</Link></li>
+  </ul>
+  </main>);
+};
+
+export default Home;
