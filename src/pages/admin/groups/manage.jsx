1af3be2452bc8526a1ad807c11819c6e41b6480e
Senayt Gobezie <senaitgobezie@gmail.com>
2023-04-30 15:00:56 +0300

    search input

--- src/pages/admin/groups/manage.jsx
+++ src/pages/admin/groups/manage.jsx
@@ -2,25 +2,25 @@ import AdminLayout from "@/components/layouts/AdminLayout/AdminLayout";
 import { allGroups } from "@/mock/groups";
 import { useState, useEffect, useCallback } from "react";
 import DataTable from "react-data-table-component";
-
+import { AiOutlineSearch } from "react-icons/ai";
 const ManageGroup = () => {
   const [groups, setGroups] = useState(allGroups);
   const [search, setSearch] = useState("");
   const [selectedRows, setSelectedRows] = useState([]);
 
   // search for groups using group name
-  // useEffect(() => {
-  //   const filteredData = allGroups.filter(
-  //     (item) =>
-  //       item.name && item.name.toLowerCase().includes(search.toLowerCase())
-  //   );
+  useEffect(() => {
+    const filteredData = allGroups.filter(
+      (item) =>
+        item.name && item.name.toLowerCase().includes(search.toLowerCase())
+    );
 
-  //   if (search) {
-  //     setGroups(filteredData);
-  //   } else {
-  //     setGroups(allGroups);
-  //   }
-  // }, [search]);
+    if (search) {
+      setGroups(filteredData);
+    } else {
+      setGroups(allGroups);
+    }
+  }, [search]);
 
   const columns = [
     {
@@ -40,15 +40,20 @@ const ManageGroup = () => {
 
   return (
     <AdminLayout>
-      <div className="grid grid-cols-3">
-        <div className="flex flex-col col-span-2 gap-4">
-          <div className="flex">
-            <h1>Manage Groups</h1>
-            <input
-              type="search"
-              value={search}
-              onChange={(e) => setGroups(e.target.value)}
-            />
+      <div className="grid grid-cols-3 gap-x-6">
+        <div className="col-span-2">
+          <div className="flex items-center justify-between mb-4">
+            <h1 className="text-2xl font-semibold">Manage Groups</h1>
+            <div className="flex pr-2 bg-white border-gray-700 rounded-md ">
+              <input
+                type="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="py-1 pl-2 bg-transparent outline-none"
+                placeholder="search from groups"
+              />
+              <AiOutlineSearch className="w-6 h-auto" />
+            </div>
           </div>
           <DataTable
             columns={columns}
