40da5bb1bef1ef9503b8a67abfbca3a798412123
Senayt Gobezie <senaitgobezie@gmail.com>
2023-04-30 15:23:38 +0300

    select group for multiple and none

--- src/pages/admin/groups/manage.jsx
+++ src/pages/admin/groups/manage.jsx
@@ -1,8 +1,9 @@
 import AdminLayout from "@/components/layouts/AdminLayout/AdminLayout";
 import { allGroups } from "@/mock/groups";
+import Link from "next/link";
 import { useState, useEffect, useCallback } from "react";
 import DataTable from "react-data-table-component";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineClose, AiOutlinePlus, AiOutlineSearch } from "react-icons/ai";
 const ManageGroup = () => {
   const [groups, setGroups] = useState(allGroups);
   const [search, setSearch] = useState("");
@@ -40,17 +41,23 @@ const ManageGroup = () => {
 
   return (
     <AdminLayout>
-      <div className="grid grid-cols-3 gap-x-6">
+      <div className="grid min-h-full grid-cols-3 gap-x-6">
         <div className="col-span-2">
+          <h1 className="mb-4 text-3xl font-semibold">Manage Groups</h1>
           <div className="flex items-center justify-between mb-4">
-            <h1 className="text-2xl font-semibold">Manage Groups</h1>
-            <div className="flex pr-2 bg-white border-gray-700 rounded-md ">
+            <Link
+              href="/admin/groups/create"
+              className="flex items-center justify-center gap-2 px-4 py-2 text-base font-semibold rounded-lg bg-primary hover:bg-secondary"
+            >
+              <AiOutlinePlus /> Create Group
+            </Link>
+            <div className="flex pr-4 bg-white border-gray-700 rounded-md ">
               <input
                 type="search"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                className="py-1 pl-2 bg-transparent outline-none"
-                placeholder="search from groups"
+                className="py-2 pl-4 bg-transparent outline-none"
+                placeholder="Search from groups"
               />
               <AiOutlineSearch className="w-6 h-auto" />
             </div>
@@ -63,8 +70,39 @@ const ManageGroup = () => {
             pagination
           />
         </div>
-        <div>
-          <pre>{JSON.stringify(selectedRows, null, 4)}</pre>
+        <div className="border-l-4 border-primary">
+          {/* <pre>{JSON.stringify(selectedRows, null, 4)}</pre> */}
+          {/* if no row is selected */}
+          {selectedRows.length === 0 && (
+            <div className="flex items-center justify-center w-full h-full text-xl">
+              <p>Select group to see details</p>
+            </div>
+          )}
+          {/* if multiple rows are selected */}
+          {selectedRows.length > 1 && (
+            <>
+              <h3 className="flex justify-between px-2 pb-4 text-xl font-semibold">
+                Selected Groups
+                <button className="flex items-center gap-1 px-2 py-1 text-base text-white bg-red-600 rounded-lg hover:bg-red-500">
+                  <AiOutlineClose className="w-5 h-auto" />
+                  Delete All
+                </button>
+              </h3>
+              <ul className="flex flex-col gap-2 px-2">
+                {selectedRows.map((row, index) => (
+                  <li
+                    key={index}
+                    className="flex justify-between px-4 py-2 bg-white rounded-lg shadow-sm shadow-black"
+                  >
+                    <p>{row.name}</p>
+                    <button className="p-1 text-white bg-red-600 rounded-lg hover:bg-red-500">
+                      <AiOutlineClose />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </div>
       </div>
     </AdminLayout>
