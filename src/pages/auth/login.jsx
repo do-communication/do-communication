fa438d8a0fbbba7ec7c7009abab8fbeb069fc85d
Kalkidan Tadu <lilykali40@gmail.com>
2023-05-02 23:40:28 -0700

    Authentication

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -4,9 +4,69 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
 import { useState } from "react";
+import { useAuth } from "../../../context/AuthContext";
+import Router from "next/router";
+import { auth } from "../../../config/firebase";
+const router = Router
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const { user, logIn, signInGoogle, provider } = useAuth()
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleLogin = async (e) =>{
+    e.preventDefault()
+    // const form = document.getElementById('loginForm')
+    try{
+      await logIn(data.email, data.password)
+      document.querySelector('.error-txt').innerHtml = ""
+      router.push('/')
+    }catch(err){
+      // if (err.message == "(auth/wrong-password)."){
+      //   setData({
+      //     ...data,
+      //     password: ''
+      //   })
+      //   document.getElementById("password").value={};
+      //   document.getElementById("password").placeholder="Your Password is incorrect";
+      //   // document.getElementById("password").value
+      // }else{
+        // const form = document.getElementById('loginForm')
+        document.querySelector('.error-txt').innerHtml = err.code
+        
+      // }
+    }
+    console.log(data)
+  }
+  
+  const handleLoginGoogle = async (e) =>{
+    e.preventDefault()
+    // const form = document.getElementById('loginForm')
+    try{
+      await signInGoogle(auth, provider)
+      form.querySelector('.error-txt').innerHtml = ""
+      router.push('/')
+    }catch(err){
+      // if (err.message == "auth/wrong-password"){
+      //   setData({
+      //     ...data,
+      //     password: ''
+      //   })
+      //   document.getElementById("password").value={};
+      //   document.getElementById("password").placeholder="Your Password is incorrect";
+      //   // document.getElementById("password").value
+      // }else{
+      //   console.log(err)
+      // }
+      // console.log(err)
+      // const form = document.getElementById('loginForm')
+      form.querySelector('.error-txt').innerHtml = err.message
+    }
+    console.log(data)
+  }
 
   return (
     <AuthLayout title="Login">
@@ -15,6 +75,12 @@ const Login = () => {
         <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <MdOutlineEmail className="w-10 text-gray-600 pl-2 h-auto" />
           <input
+            onChange={(e) => setData({
+              ...data,
+              email : e.target.value
+            })
+            }
+            value={data.email}
             type="email"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
             placeholder="Enter your email"
@@ -25,8 +91,15 @@ const Login = () => {
         {/*password input */}
         <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pr-5 pl-5">
           <input
+            onChange={(e) => setData({
+              ...data,
+              password : e.target.value
+            })
+            }
+            value={data.password}
             type={showPassword ? "text" : "password"}
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-[52px] outline-none"
+            id="password"
             placeholder="Enter your password"
             size="lg"
           />
@@ -50,13 +123,14 @@ const Login = () => {
         </Link>
       </div>
       <div className="flex flex-col gap-4">
-        <button className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-4 rounded-full shadow-sm shadow-black hover:brightness-95">
+        <button onClick={handleLogin} className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-4 rounded-full shadow-sm shadow-black hover:brightness-95">
           Sign in
         </button>
+        <h1 className="error-txt text-red-700"></h1>
         <fieldset class="border-t border-black">
           <legend class="mx-auto px-4 text-black text-lg">or</legend>
         </fieldset>
-        <button className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-4 rounded-full shadow-sm shadow-black hover:brightness-95">
+        <button onClick={handleLoginGoogle} className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-4 rounded-full shadow-sm shadow-black hover:brightness-95">
           Sign in with &nbsp; <FcGoogle className="h-auto w-6" />
         </button>
       </div>
--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -6,15 +6,43 @@ import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
 import { useState } from "react";
 import AuthLayout from "@/components/layouts/AuthLayout/AuthLayout";
+import { useAuth } from "../../../context/AuthContext";
+import Router from 'next/router'
+const router = Router
 const Signup = () => {
-    const [showPassword, setShowPassword] = useState(false);
+  const { user, signUp } = useAuth()
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [data, setData] = useState({
+    name:'',
+    companyName:'',
+    email: '',
+    password: ''
+  })
+
+  const handleSignUp = async (e) =>{
+    e.preventDefault()
+    try{
+      await signUp(data.email, data.password) 
+      router.push('/')    
+    }catch(err){
+      console.log(err)
+    }
+    console.log(data)
+  }
+
   return <AuthLayout title="Sign up">
       <div className="flex flex-col gap-4">
         {/* name input */}
         <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <MdOutlinePersonOutline className="w-10 text-gray-600 pl-2 h-auto" />
-          <input
-            type="email"
+          <input 
+            onChange={(e) => setData({
+              ...data,
+              name: e.target.value
+            })}
+            value={data.name}
+            type="text"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
             placeholder="Enter your name"
             size="lg"
@@ -24,6 +52,11 @@ const Signup = () => {
            <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <HiOutlineBuildingOffice2 className="w-10 text-gray-600 pl-2 h-auto" />
           <input
+            onChange={(e) => setData({
+              ...data,
+              companyName: e.target.value
+            })}
+            value={data.companyName}
             type="text"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
             placeholder="Enter your company name"
@@ -34,6 +67,11 @@ const Signup = () => {
         <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <MdOutlineEmail className="w-10 text-gray-600 pl-2 h-auto" />
           <input
+            onChange={(e) => setData({
+              ...data,
+              email: e.target.value
+            })}
+            value={data.email}
             type="email"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
             placeholder="Enter your email"
@@ -44,6 +82,11 @@ const Signup = () => {
         {/*password input */}
         <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pr-5 pl-5">
           <input
+            onChange={(e) => setData({
+              ...data,
+              password: e.target.value
+            })}
+            value={data.password}
             type={showPassword ? "text" : "password"}
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-[52px] outline-none"
             placeholder="Enter your password"
@@ -63,7 +106,7 @@ const Signup = () => {
         </div>
       </div>
       <div className="flex flex-col gap-3">
-        <button className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-3 rounded-full shadow-sm shadow-black hover:brightness-95">
+        <button onClick={handleSignUp} className="w-full text-lg font-semibold flex items-center justify-center bg-primary py-3 rounded-full shadow-sm shadow-black hover:brightness-95">
           Sign in
         </button>
         <fieldset class="border-t border-black">
