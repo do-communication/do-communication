23d46965d40a155e86fda9c4dcf0ea6c1c0defa7
Kalkidan Tadu <lilykali40@gmail.com>
2023-05-11 23:01:11 -0700

    Authentication and error handling

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -19,28 +19,142 @@ const Signup = () => {
     email: '',
     password: ''
   })
+  const emailSignUp = document.getElementById("emailSignUp");
+  const passwordSignUp = document.getElementById("passwordSignUp");
+  const divemail = document.getElementById("divemail");
+  const divpassword = document.getElementById("divpassword");
+  const companyInput = document.getElementById("companyInput");
+  const nameInput = document.getElementById("nameInput");
+  const namediv = document.getElementById("namediv");
+  const companydiv = document.getElementById("companydiv");
 
+  const handleEmailChange = (e) =>{
+    e.preventDefault();
+    setData({
+      ...data,
+      email: e.target.value
+    })
+    if(divemail && divemail.classList.contains("ring-red-600")){
+      divemail.classList.remove("ring-red-600");
+      divemail.classList.remove("ring-2");
+      divemail.classList.add("ring-blue-300");
+      emailSignUp.placeholder = "Enter your email";
+    }
+  }
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+
+    setData({
+      ...data,
+      password: e.target.value
+    })
+
+    if(divpassword && divpassword.classList.contains("ring-red-600")){
+      divpassword.classList.remove("ring-red-600");
+      divpassword.classList.remove("ring-2");
+      divpassword.classList.add("ring-blue-300");
+      passwordSignUp.placeholder = "Enter Your Password";
+    }
+  }
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      name: e.target.value
+    })
+
+    if(namediv && namediv.classList.contains("ring-red-600")){
+      namediv.classList.remove("ring-red-600");
+      namediv.classList.remove("ring-2");
+      namediv.classList.add("ring-blue-300");
+      nameInput.placeholder = "Enter Your Name";
+    }
+  }
+  const handleComapnyChange = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      companyName: e.target.value
+    })
+
+    if(companydiv && companydiv.classList.contains("ring-red-600")){
+      companydiv.classList.remove("ring-red-600");
+      companydiv.classList.remove("ring-2");
+      companydiv.classList.add("ring-blue-300");
+      companyInput.placeholder = "Enter Your company name";
+    }
+  }
   const handleSignUp = async (e) =>{
     e.preventDefault()
+    if (namediv && nameInput.value == ""){
+      nameInput.placeholder = "Your name is required";
+      namediv.classList.remove("ring-blue-300");
+      namediv.classList.add("ring-red-600");
+      namediv.classList.add("ring-2");
+    }
+    if (companydiv && companyInput.value == ""){
+      companyInput.placeholder = "Your company name is required";
+      companydiv.classList.remove("ring-blue-300");
+      companydiv.classList.add("ring-red-600");
+      companydiv.classList.add("ring-2");
+    }
+    if (divemail && emailSignUp.value == ""){
+      emailSignUp.placeholder = "Your email is required";
+      divemail.classList.remove("ring-blue-300");
+      divemail.classList.add("ring-red-600");
+      divemail.classList.add("ring-2");
+    }
+    if (divpassword && passwordSignUp.value == ""){
+      passwordSignUp.placeholder = "Password is required";
+      divpassword.classList.remove("ring-blue-300");
+      divpassword.classList.add("ring-red-600");
+      divpassword.classList.add("ring-2");
+    }
+    if(passwordSignUp.value != "" && emailSignUp.value != "" && nameInput.value != "" && companyInput.value != ""){
     try{
       await signUp(data.email, data.password) 
       router.push('/')    
     }catch(err){
-      console.log(err)
+      if(err.message == "Firebase: Password should be at least 6 characters (auth/weak-password)."){
+        passwordSignUp.value="";
+        passwordSignUp.placeholder = "Password must be at least 6 characters";
+        divpassword.classList.remove("ring-blue-300");
+        divpassword.classList.add("ring-red-600");
+        divpassword.classList.add("ring-2");
+      }
+      else if (err.message == "Firebase: Error (auth/email-already-in-use)."){
+        if (emailSignUp){
+        emailSignUp.value = "";
+      }
+        emailSignUp.placeholder = "Email already in use";
+        divemail.classList.remove("ring-blue-300");
+        divemail.classList.add("ring-red-600");
+        divemail.classList.add("ring-2");
+      }
+      else{
+        if (emailSignUp){
+          emailSignUp.value = "";
+        }
+        emailSignUp.placeholder = "The email is invalid";
+        divemail.classList.remove("ring-blue-300");
+        divemail.classList.add("ring-red-600");
+        divemail.classList.add("ring-2");
+
     }
-    console.log(data)
+      console.log(err.message);
+  }
+    console.log(data);
+  }
   }
 
   return <AuthLayout title="Sign up">
       <div className="flex flex-col gap-4">
         {/* name input */}
-        <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
+        <div id="namediv" className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <MdOutlinePersonOutline className="w-10 text-gray-600 pl-2 h-auto" />
           <input 
-            onChange={(e) => setData({
-              ...data,
-              name: e.target.value
-            })}
+            onChange={handleNameChange}
+            id="nameInput"
             value={data.name}
             type="text"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
@@ -49,14 +163,12 @@ const Signup = () => {
           />
         </div>
         {/* company name input */}
-           <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
+           <div id="companydiv" className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <HiOutlineBuildingOffice2 className="w-10 text-gray-600 pl-2 h-auto" />
           <input
-            onChange={(e) => setData({
-              ...data,
-              companyName: e.target.value
-            })}
+            onChange={handleComapnyChange}
             value={data.companyName}
+            id="companyInput"
             type="text"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
             placeholder="Enter your company name"
@@ -64,13 +176,11 @@ const Signup = () => {
           />
         </div>
         {/* email input */}
-        <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
+        <div id="divemail" className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pl-5">
           <MdOutlineEmail className="w-10 text-gray-600 pl-2 h-auto" />
           <input
-            onChange={(e) => setData({
-              ...data,
-              email: e.target.value
-            })}
+            onChange={handleEmailChange}
+            id="emailSignUp"
             value={data.email}
             type="email"
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-2 outline-none"
@@ -80,12 +190,10 @@ const Signup = () => {
         </div>
 
         {/*password input */}
-        <div className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pr-5 pl-5">
+        <div id="divpassword" className="bg-white group flex rounded-3xl gap-2 hover:ring-2 active:ring-2 ring-blue-300 pr-5 pl-5">
           <input
-            onChange={(e) => setData({
-              ...data,
-              password: e.target.value
-            })}
+            onChange={handlePasswordChange}
+            id="passwordSignUp"
             value={data.password}
             type={showPassword ? "text" : "password"}
             className="bg-white text-gray-600 w-full rounded-3xl py-4 px-[52px] outline-none"
