83905c93b324ee70493b0fdcd93b5fd39b524063
Kalkidan Tadu <lilykali40@gmail.com>
2023-05-23 12:13:54 -0700

    Add members and Manage members

--- src/pages/admin/members/ManageMembers.jsx
+++ src/pages/admin/members/ManageMembers.jsx
@@ -1,8 +1,9 @@
 import AdminLayout from "@/components/layouts/AdminLayout/AdminLayout";
-import { allMembers } from "@/mock/members";
 import Link from "next/link";
 import { useState, useEffect, useCallback } from "react";
 import DataTable from "react-data-table-component";
+import { db } from "../../../../context/DbContext"
+import { doc, getDocs, getDoc, collection } from "firebase/firestore";
 import {
   AiFillDelete,
   AiFillEdit,
@@ -16,12 +17,29 @@ import { HiDocumentChartBar, HiUsers } from "react-icons/hi2";
 import { MdChecklist } from "react-icons/md";
 import { TbMessage } from "react-icons/tb";
 const ManageMembers = () => {
-  const [members, setMembers] = useState(allMembers);
+  const [allMembers, setallMembers] = useState([]);
+  const [members, setMembers] = useState([allMembers]);
   const [search, setSearch] = useState("");
   const [selectedRows, setSelectedRows] = useState([]);
 
   const [showManageGroupMenu, setShowManageGroupMenu] = useState(false);
+  const getData = async () => {
+    let arr = []
+    const all = collection(db, "KalCompany", "Users", "StaffMembers");
+    try {
+      const doc = await getDocs(all)
+      doc.forEach(d => {
+        arr.push(d.data())
+      });
 
+    } catch (err) {
+      console.log(err)
+      setMembers([{ Name: "check your connection" }])
+    }
+
+    setMembers(arr)
+    setallMembers(arr)
+  }
   // search for groups using group name
   useEffect(() => {
     const filteredData = allMembers.filter(
@@ -35,6 +53,7 @@ const ManageMembers = () => {
       setMembers(filteredData);
     } else {
       setMembers(allMembers);
+      console.log(members);
     }
   }, [search]);
 
@@ -42,32 +61,38 @@ const ManageMembers = () => {
     {
       name: "Name",
       selector: (row) => (
-        <p className="flex items-center gap-2">
-          <img
-            src={row.photo}
-            width={50}
-            height={50}
-            alt="pp"
-            className="rounded-full"
-          />
+        <p className="flex items-center justify-center gap-2"><div className="flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full">
+          {row.ProfilePic === '' ? row.Name[0] : <img
+          src={row.ProfilePic}
+          width={50}
+          height={50}
+          alt= "pp"
+          className="rounded-full"
+        />}
+        </div>
+        <div>
+        {row.Name}
+      </div></p>
 
-          {row.name}
-        </p>
       ),
       sortable: true,
     },
     {
       name: "Email",
-      selector: (row) => row.email,
+      selector: (row) => row.Email,
     },
     {
       name: "Department",
-      selector: (row) => row.department,
+      selector: (row) => row.Department,
     },
   ];
 
   const handleRowSelected = useCallback((state) => {
     setSelectedRows(state.selectedRows);
+  }, []);
+
+  useEffect(() => {
+    getData()
   }, []);
 
   return (
@@ -77,7 +102,7 @@ const ManageMembers = () => {
           <h1 className="mb-5 text-2xl font-semibold">Manage Members</h1>
           <div className="flex items-center justify-between pb-0 mb-2">
             <Link
-              href="/admin/Members/create"
+              href="/admin/members/create"
               className="flex items-center justify-center gap-2 px-4 py-2 text-base font-semibold rounded-lg bg-primary hover:bg-secondary"
             >
               <AiOutlinePlus /> Add Member
@@ -126,7 +151,7 @@ const ManageMembers = () => {
                     key={index}
                     className="flex justify-between px-4 py-2 bg-white rounded-lg shadow-sm shadow-black"
                   >
-                    <p>{row.name}</p>
+                    <p>{row.Name}</p>
                     <button className="p-1 text-white bg-red-600 rounded-lg hover:bg-red-500">
                       <AiOutlineClose />
                     </button>
@@ -183,17 +208,20 @@ const ManageMembers = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <div className="flex items-center justify-center w-20 h-20 rounded-full bg-primary">
-                  {/* <MdGroup className="w-12 h-12" /> */}
-                  <img
-                    src="/images/pp.png"
-                    className="flex items-center justify-center w-20 h-20 rounded-full bg-primary"
-                  />
+                    {selectedRows[0].ProfilePic === '' ? selectedRows[0].Name[0] : 
+                    <img
+                    src={selectedRows[0].ProfilePic}
+                    width={50}
+                    height={50}
+                    alt= "pp"
+                    className="rounded-full"
+                  />}
                 </div>
                 <h4 className="text-xl font-semibold capitalize" mt-1>
-                  {selectedRows[0].name}
+                  {selectedRows[0].Name}
                 </h4>
                 <p className="font-light text-md" mt-1>
-                  {selectedRows[0].email}
+                  {selectedRows[0].Email}
                 </p>
                 <p className="text-sm">{selectedRows[0].type}</p>
               </div>
@@ -218,7 +246,7 @@ const ManageMembers = () => {
                       </p>
                     </div>
                     <p className="w-40 truncate" title="Lidiya Solomon Tamru">
-                      Lidiya Solomon Tamru
+                      {selectedRows[0].Name}
                     </p>
                   </div>
 
@@ -229,7 +257,7 @@ const ManageMembers = () => {
                       </p>
                     </div>
                     <p className="w-40 truncate" title="Product Manager">
-                      Product Manager
+                      {selectedRows[0].Department}
                     </p>
                   </div>
                   <div className="flex items-center justify-between p-2 rounded-sm hover:bg-opacity-25 hover:bg-secondary">
@@ -239,7 +267,7 @@ const ManageMembers = () => {
                       </p>
                     </div>
                     <p className="w-40 truncate" title="Addis Ababa/Ethiopia">
-                      Addis Ababa/Ethiopia
+                      {selectedRows[0].Address}
                     </p>
                   </div>
                   <div className="flex items-center justify-between p-2 rounded-sm hover:bg-opacity-25 hover:bg-secondary">
@@ -249,7 +277,7 @@ const ManageMembers = () => {
                       </p>
                     </div>
                     <p className="w-40 truncate" title="+251910******">
-                      +251910******
+                      {selectedRows[0].PhoneNumber}
                     </p>
                   </div>
                 </ul>
@@ -259,9 +287,9 @@ const ManageMembers = () => {
                 <h3 className="p-2 text-lg font-semibold text-center">
                   Joined Groups
                 </h3>
-
+                
                 <ul className="flex flex-col gap-2 overflow-y-auto max-h-64">
-                  <Link
+                  {/* <Link
                     href="/admin/memebers/{userId}"
                     className="flex items-center justify-between p-2 rounded-md hover:bg-opacity-25 hover:bg-secondary"
                   >
@@ -305,7 +333,21 @@ const ManageMembers = () => {
                     >
                       <BsEye /> View
                     </button>
-                  </Link>
+                  </Link> */}
+                  {selectedRows[0].GroupId && selectedRows[0].GroupId.map((row, index) => (
+                  <li
+                    key={index}
+                    className="flex justify-between px-4 py-2 bg-white rounded-lg shadow-sm shadow-black"
+                  >
+                    <p>{row}</p>
+                    <button
+                      type="submit"
+                      className="flex items-center gap-1 px-2 text-white rounded-lg bg-primary hover:bg-secondary"
+                    >
+                      <BsEye /> View
+                    </button>
+                  </li>
+                ))}
                 </ul>
               </div>
             </div>
--- src/pages/admin/members/create.jsx
+++ src/pages/admin/members/create.jsx
@@ -1,7 +1,174 @@
 import AdminLayout from "@/components/layouts/AdminLayout/AdminLayout";
-
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../../../context/DbContext";
+import Router from 'next/router';
+const router = Router
 const AddMember = () => {
-  
+  const [data, setData] = useState({
+    Name: '',
+    Address: '',
+    Email: '',
+    Gender: '',
+    Department: '',
+    PhoneNumber: '',
+    DateOfBirth: new Date("10/10/2030"),
+    ProfilePic: '',
+    RegisteredAt: new Date(),
+    GroupId: []
+  });
+  const select = document.getElementById('selectGender');
+  const email = document.getElementById('email');
+  const name = document.getElementById('full_name');
+  const address = document.getElementById('address');
+  const phone = document.getElementById('phoneNumber');
+  const department = document.getElementById('department');
+  const dob = document.getElementById('dob');
+  const date = document.getElementById('date');
+  const pho = document.getElementById('pho');
+  const dep = document.getElementById('dep');
+
+  select && select.addEventListener('change', function handleChange(event) {
+    setData({
+      ...data,
+      Gender : event.target.value
+    });
+    if (select && select.classList.contains("ring-red-600")) {
+      select.classList.remove("ring-red-600");
+      select.classList.remove("ring-2");
+    }
+  });
+  const handleName = (e) =>{
+    e.preventDefault();
+    setData({
+      ...data,
+      Name: e.target.value
+    })
+    if (name && name.classList.contains("ring-red-600")) {
+      name.classList.remove("ring-red-600");
+      name.classList.remove("ring-2");
+      name.placeholder = "";
+    }
+  };
+  const handleEmail = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      Email: e.target.value
+    })
+    if (email && email.classList.contains("ring-red-600")) {
+      email.classList.remove("ring-red-600");
+      email.classList.remove("ring-2");
+      email.placeholder = "email@employee.com";
+    }
+  };
+  const handleAddress = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      Address: e.target.value
+    })
+    if (address && address.classList.contains("ring-red-600")) {
+      address.classList.remove("ring-red-600");
+      address.classList.remove("ring-2");
+      address.placeholder = "";
+    }
+  };
+  const handleDepartment = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      Department: e.target.value
+    })
+    if (department && department.classList.contains("ring-red-600")) {
+      department.classList.remove("ring-red-600");
+      department.classList.remove("ring-2");
+      department.placeholder = "Department";
+    }
+  };
+  const handlePhone = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      PhoneNumber: e.target.value
+    })
+    if (phone && phone.classList.contains("ring-red-600")) {
+      phone.classList.remove("ring-red-600");
+      phone.classList.remove("ring-2");
+      phone.placeholder = "+251 9";
+    }
+  };
+  const handleDob = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      DateOfBirth: e.target.value
+    })
+    if (dob && dob.classList.contains("ring-red-600")) {
+      dob.classList.remove("ring-red-600");
+      dob.classList.remove("ring-2");
+      dob.placeholder = "MM/DD/YYYY";
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email && data.Email == "") {
+      email.placeholder = "You should enter an Email address";
+      email.classList.add("ring-red-600");
+      email.classList.add("ring-2");
+    }
+    if (name && data.Name == "") {
+      name.placeholder = "You should enter Full Name";
+      name.classList.add("ring-red-600");
+      name.classList.add("ring-2");
+    }
+    if (address && data.Address == "") {
+      address.placeholder = "You should enter an Address";
+      address.classList.add("ring-red-600");
+      address.classList.add("ring-2");
+    }
+    if (department && data.Department == "") {
+      department.placeholder = "You should enter a Department";
+      department.classList.add("ring-red-600");
+      department.classList.add("ring-2");
+    }
+    if (phone && data.PhoneNumber == "") {
+      phone.placeholder = "You should fill out employee's phone number";
+      phone.classList.add("ring-red-600");
+      phone.classList.add("ring-2");
+    }
+    if (select && (data.Gender == "null" || data.Gender === '')){
+      select.classList.add("ring-red-600");
+      select.classList.add("ring-2");
+    }
+    if (dob && data.DateOfBirth > new Date()){
+      dob.classList.add("ring-red-600");
+      dob.classList.add("ring-2");
+    }
+    if(data.Name != "" && data.Email != "" && data.PhoneNumber != "" && data.Address != "" && data.Department != "" && data.DateOfBirth != "" && data.Gender != "null" ){
+    await addDoc(collection(db, "KalCompany", "Users", "StaffMembers"), data);
+    router.push("/admin/members/ManageMembers");}
+  };
+  const handleClear = (e) => {
+    name.value = "";
+    data.Name = "";
+    name.placeholder = "";
+    email.value = "";
+    data.Email = "";
+    email.placeholder = "employee@gmail.com";
+    address.value = "";
+    data.Address = "";
+    address.placeholder = "";
+    pho.value = "";
+    data.PhoneNumber = "";
+    pho.placeholder = "+251 9";
+    date.value = null;
+    data.DateOfBirth = new Date("10/10/2030")
+    date.placeholder = "MM/DD/YYYY";
+    dep.value = "";
+    data.Department = "";
+    dep.placeholder = "Department";
+  };
   return (
     <AdminLayout>
     <div className="min-h-screen p-6 pt-8 bg-gray-100 flex  justify-center">
@@ -21,6 +188,8 @@ const AddMember = () => {
                   <div className="md:col-span-3">
                     <label for="full_name">Full Name</label>
                     <input
+                      onChange={handleName}
+                      value={data.Name}
                       type="text"
                       name="full_name"
                       id="full_name"
@@ -28,42 +197,49 @@ const AddMember = () => {
                     />
                   </div>
 
-                  <div className="md:col-span-3">
+                  <div id="emailDiv" className="md:col-span-3">
                     <label for="email">Email Address</label>
                     <input
+                      onChange={handleEmail}
                       type="text"
                       name="email"
+                      value={data.Email}
                       id="email"
                       className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                       placeholder="email@employee.com"
                     />
                   </div>
 
-                  <div className="md:col-span-3">
+                  <div id="addressDiv" className="md:col-span-3">
                     <label for="address">Address</label>
                     <input
+                      onChange={handleAddress}
                       type="text"
                       name="address"
+                      value={data.Address}
                       id="address"
                       className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                       placeholder=""
                     />
                   </div>
 
-                  <div className="md:col-span-3">
+                  <div id="genderDiv" className="md:col-span-3">
                     <label for="Gender">Gender</label>
-                    <select className="h-10 border mt-1 rounded px-4 w-full bg-gray-50">
+                    <select id="selectGender" className="h-10 border mt-1 rounded px-4 w-full bg-gray-50">
                       <option value="null">Select gender</option>
-                      <option value="f">Female</option>
+                      <option  value="f">Female</option>
                       <option value="m">Male</option>
                     </select>
                   </div>
 
-                  <div className="md:col-span-3">
+                  <div id="phoneDiv" className="md:col-span-3">
                     <label for="country">Phone Number</label>
-                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                    <div id="phoneNumber" className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                       <input
+                        onChange={handlePhone}
+                        value={data.PhoneNumber}
                         type="tel"
+                        id = "pho"
                         pattern="[789][0-9]{9}" required
                         name="country"
                         placeholder="+251 9 "
@@ -75,23 +251,27 @@ const AddMember = () => {
 
                   <div className="md:col-span-3">
                     <label for="state">Department</label>
-                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                    <div id="department" className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                       <input
+                        onChange={handleDepartment}
                         name="state"
-                        id="state"
+                        id = "dep"
+                        value={data.Department}
                         placeholder="Department"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                       />
                     </div>
                   </div>
 
-                  <div className="md:col-span-3">
+                  <div  className="md:col-span-3">
                     <label for="state">Date of Birth</label>
-                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                    <div id="dob" className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                     <input type="date"
+                        id = "date"
+                        onChange={handleDob}
+                        value={data.DateOfBirth}
                         onfocus="(this.type='date')" name="DB"
-                        placeholder="MM/DD/YYYY"
-                        id="state"
+                        placeholder="MM/DD/YYYY"    
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"/>
                     </div>
                   </div>
@@ -99,10 +279,10 @@ const AddMember = () => {
                   <div className="md:col-span-6 text-right ml-auto">
                     <div className="inline-flex items-end justify-end">
                       <div className="flex-row gap-10 pt-8">
-                        <button className="bg-gray-300 hover:bg-primary text-balck  font-bold py-2 px-4 mr-6 rounded border-b-2">
+                        <button onClick={handleClear} className="bg-gray-300 hover:bg-primary text-balck  font-bold py-2 px-4 mr-6 rounded border-b-2">
                           Cancel
                         </button>
-                        <button className="bg-primary hover:bg-bold text-white font-bold py-2 px-4 rounded">
+                        <button onClick={handleSubmit} className="bg-primary hover:bg-bold text-white font-bold py-2 px-4 rounded">
                           Submit
                         </button>
                       </div>
