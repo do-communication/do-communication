d492f5186960e49db752a7a1dd86c0549d285572
Dagmawi Muluwork <dagmawimuluwork@gmail.com>
2023-05-24 21:59:12 +0300

    nothing new

--- src/mock/members.js
+++ src/mock/members.js
@@ -1,37 +1,37 @@
 export const allMembers = [
   {
     photo: "/images/pp.png",
-    Name: "member 1",
+    name: "member 1",
     email: "Member1@gmail.com",
     department: "Reseption",
   },
   {
     photo: "/images/pp.png",
-    Name: "member 2",
+    name: "member 2",
     email: "Member2@gmail.com",
     department: "Designer",
   },
   {
     photo: "/images/pp.png",
-    Name: "member 3",
+    name: "member 3",
     email: "Member3@gmail.com",
     department: "Manager",
   },
   {
     photo: "/images/pp.png",
-    Name: "member 4",
+    name: "member 4",
     email: "Member4@gmail.com",
     department: "Accountant",
   },
   {
     photo: "/images/pp.png",
-    Name: "member 5",
+    name: "member 5",
     email: "Member5@gmail.com",
     department: "designer",
   },
   {
     photo: "/images/pp.png",
-    Name: "member 6",
+    name: "member 6",
     email: "Member6@gmail.com",
     department: "designer",
   },
