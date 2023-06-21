48305294a9a5e2ce6c01055ce6e24e687c8bfb4d
Anani Samuel <ananisamuelhope@gmail.com>
2023-06-21 23:04:19 +0300

    user side letter

--- src/pages/user/letters/letter.jsx
+++ src/pages/user/letters/letter.jsx
@@ -1,6 +1,10 @@
 import UserLayout from "@/components/layouts/UserLayout/UserLayout";
 import { toast } from "react-toastify";
-import { useState } from "react";
+import { doc, getDocs, getDoc, addDoc, collection, updateDoc } from "firebase/firestore";
+import { useState, useEffect, useRef } from "react";
+import { auth } from "../../../../config/firebase";
+const user = auth.currentUser;
+import { db } from "../../../../context/DbContext";
 import { RiAttachment2 } from "react-icons/ri";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
@@ -12,14 +16,39 @@ const QuillNoSSRWrapper = dynamic(import("react-quill"), {
 
 const AddReport = () => {
   const [reportDetail, setReportDetail] = useState("");
-  const handleSubmit = (e) => {
+  const [data, setData] = useState({
+    To: '',
+    Address: '',
+    Subject: '',
+    Body: '', 
+    From: user.displayName
+  });
+  const to = document.getElementById('to');
+  const address = document.getElementById('address');
+  const body = document.getElementById('body');
+  const subject = document.getElementById('subject');
+  const handleClear = () =>{
+    // setReportDetail("");
+    to.value = '';
+    to.placeholder = "enter recipient name";
+    address.value = '';
+    address.placeholder = 'enter recipient address';
+    body.placeholder = 'Write body part of the letter';
+    subject.value = '';
+    subject.placeholder = 'enter report title';
+  }
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    console.log("submit");
-
+    await addDoc(collection(db, "KalCompany", "Letter", "Letter"), data);
+    handleClear();
     toast.success("Letter Created successfully");
     // create it in firestore
     // redirect to /letters/[letterId]
   };
+  useEffect(() => {
+    const value = {...data, Body:reportDetail}
+    setData(value);
+  }, [reportDetail])
 
   return (
     <UserLayout>
@@ -48,8 +77,10 @@ const AddReport = () => {
                       type="text"
                       name="report_title"
                       className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
-                      id="task_title"
+                      id="to"
                       placeholder="enter recipient name"
+                      value={data.To}
+                      onChange={(e) => {setData({...data, To:e.target.value})}}
                     />
                   </div>
                   <div className="md:col-span-6">
@@ -58,8 +89,10 @@ const AddReport = () => {
                       type="text"
                       name="report_title"
                       className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
-                      id="task_title"
+                      id="address"
                       placeholder="enter recipient address"
+                      value={data.Address}
+                      onChange={(e) => {setData({...data, Address:e.target.value})}}
                     />
                   </div>
                   <div className="grid grid-cols-1 gap-6 text-sm gap-y-5 md:grid-cols-6">
@@ -69,8 +102,12 @@ const AddReport = () => {
                         type="text"
                         name="report_title"
                         className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
-                        id="task_title"
+                        id="subject"
                         placeholder="enter report title"
+                        value={data.Subject}
+                        onChange={(e) => {
+                          setData({...data, Subject:e.target.value});
+                          }}
                       />
                     </div>
 
@@ -78,8 +115,12 @@ const AddReport = () => {
                       <label for="detail">Body</label>
                       <QuillNoSSRWrapper
                         theme="snow"
+                        id="body"
                         value={reportDetail}
-                        onChange={setReportDetail}
+                        onChange={(e)=>{
+                          setReportDetail(e.slice(3, e.lastIndexOf("</p>")));
+                        }}
+                        
                         placeholder="Write body part of the letter"
                         style={{ height: 180 }}
                       />
@@ -88,7 +129,9 @@ const AddReport = () => {
                     <div className="ml-auto text-right md:col-span-6">
                       <div className="inline-flex items-end justify-end">
                         <div className="flex-row gap-10 ">
-                          <button className="px-4 py-2 mr-6 font-bold bg-gray-300 border-b-2 rounded hover:bg-primary text-balck">
+                          <button 
+                          onClick={handleClear}
+                          className="px-4 py-2 mr-6 font-bold bg-gray-300 border-b-2 rounded hover:bg-primary text-balck">
                             Cancel
                           </button>
                           <button
