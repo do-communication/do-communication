2a15b9bd7bc9983e0b7111f58147ede2df333d3b
Senayt Gobezie <senaitgobezie@gmail.com>
2023-06-24 13:32:15 +0300

    add tinymce editor

--- src/pages/admin/letters/create.jsx
+++ src/pages/admin/letters/create.jsx
@@ -14,15 +14,15 @@ const user = auth.currentUser;
 import { db } from "../../../../context/DbContext";
 import { RiAttachment2 } from "react-icons/ri";
 import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
 
-const QuillNoSSRWrapper = dynamic(import("react-quill"), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
-});
+const ClientOnlyEditor = dynamic(
+  () => import("@tinymce/tinymce-react").then((module) => module.Editor),
+  { ssr: false, loading: () => <p>Loading ...</p> }
+);
 
-const AddReport = () => {
+const AddLetter = () => {
   const [reportDetail, setReportDetail] = useState("");
+  const editorRef = useRef(null);
   const [data, setData] = useState({
     To: "",
     Address: "",
@@ -32,7 +32,6 @@ const AddReport = () => {
   });
   const to = document.getElementById("to");
   const address = document.getElementById("address");
-  const body = document.getElementById("body");
   const subject = document.getElementById("subject");
   const handleClear = () => {
     // setReportDetail("");
@@ -40,13 +39,19 @@ const AddReport = () => {
     to.placeholder = "enter recipient name";
     address.value = "";
     address.placeholder = "enter recipient address";
-    body.placeholder = "Write body part of the letter";
     subject.value = "";
     subject.placeholder = "enter report title";
+    editorRef.current.setContent("");
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await addDoc(collection(db, "KalCompany", "Letter", "Letter"), data);
+
+    const newLetter = {
+      ...data,
+      Body: editorRef.current.getContent(),
+    };
+
+    await addDoc(collection(db, "KalCompany", "Letter", "Letter"), newLetter);
     handleClear();
     toast.success("Letter Created successfully");
     // create it in firestore
@@ -59,14 +64,14 @@ const AddReport = () => {
 
   return (
     <AdminLayout>
-      <div className="flex min-h-screen justify-center bg-gray-100 p-6 pt-2">
-        <div className="container mx-auto max-w-screen-lg">
+      <div className="flex justify-center min-h-screen p-6 pt-2 bg-gray-100">
+        <div className="container max-w-screen-lg mx-auto">
           <form>
-            <h2 className="pb-4 pt-0 text-xl font-semibold text-gray-600">
+            <h2 className="pt-0 pb-4 text-xl font-semibold text-gray-600">
               Write a Letter
             </h2>
-            <div className="mb-6 rounded bg-white p-4 px-4 shadow-sm md:p-8">
-              <div className="grid grid-cols-1 gap-4 gap-y-2 text-sm lg:grid-cols-3">
+            <div className="p-4 px-4 mb-6 bg-white rounded shadow-sm md:p-8">
+              <div className="grid grid-cols-1 gap-4 text-sm gap-y-2 lg:grid-cols-3">
                 <div className="text-gray-600">
                   <p className="pb-3 pl-4 text-lg font-medium">Letter</p>
                   <img
@@ -83,7 +88,7 @@ const AddReport = () => {
                     <input
                       type="text"
                       name="report_title"
-                      className="mt-1 h-10 w-full rounded border bg-gray-50 px-4"
+                      className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
                       id="to"
                       placeholder="enter recipient name"
                       value={data.To}
@@ -97,7 +102,7 @@ const AddReport = () => {
                     <input
                       type="text"
                       name="report_title"
-                      className="mt-1 h-10 w-full rounded border bg-gray-50 px-4"
+                      className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
                       id="address"
                       placeholder="enter recipient address"
                       value={data.Address}
@@ -106,13 +111,13 @@ const AddReport = () => {
                       }}
                     />
                   </div>
-                  <div className="grid grid-cols-1 gap-6 gap-y-5 text-sm md:grid-cols-6">
+                  <div className="grid grid-cols-1 gap-6 text-sm gap-y-5 md:grid-cols-6">
                     <div className="md:col-span-6">
                       <label htmlFor="full_name"> Subject:</label>
                       <input
                         type="text"
                         name="report_title"
-                        className="mt-1 h-10 w-full rounded border bg-gray-50 px-4"
+                        className="w-full h-10 px-4 mt-1 border rounded bg-gray-50"
                         id="subject"
                         placeholder="enter report title"
                         value={data.Subject}
@@ -124,7 +129,7 @@ const AddReport = () => {
 
                     <div className="md:col-span-6">
                       <label htmlFor="detail">Body</label>
-                      <QuillNoSSRWrapper
+                      {/* <QuillNoSSRWrapper
                         theme="snow"
                         id="body"
                         value={reportDetail}
@@ -133,6 +138,38 @@ const AddReport = () => {
                         }}
                         placeholder="Write body part of the letter"
                         style={{ height: 180 }}
+                      /> */}
+                      <ClientOnlyEditor
+                        onInit={(evt, editor) => (editorRef.current = editor)}
+                        apiKey="g1yw7n29lj6bkf1dco2yof3tac3lznaq0g325pdit2lczvxk"
+                        init={{
+                          height: 500,
+                          menubar: false,
+                          plugins: [
+                            "advlist",
+                            "autolink",
+                            "lists",
+                            "link",
+                            "image",
+                            "charmap",
+                            "preview",
+                            "anchor",
+                            "searchreplace",
+                            "visualblocks",
+                            "fullscreen",
+                            "insertdatetime",
+                            "media",
+                            "table",
+                            "help",
+                            "wordcount",
+                          ],
+                          toolbar:
+                            "undo redo | casechange blocks | bold italic backcolor | " +
+                            "alignleft aligncenter alignright alignjustify | " +
+                            "bullist numlist checklist outdent indent | removeformat | a11ycheck code table help",
+                          content_style:
+                            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                        }}
                       />
                     </div>
                     <div className="mt-10 md:col-span-6 "></div>
@@ -141,12 +178,13 @@ const AddReport = () => {
                         <div className="flex-row gap-10 ">
                           <button
                             onClick={handleClear}
-                            className="text-balck mr-6 rounded border-b-2 bg-gray-300 px-4 py-2 font-bold hover:bg-primary"
+                            type="button"
+                            className="px-4 py-2 mr-6 font-bold bg-gray-300 border-b-2 rounded text-balck hover:bg-primary"
                           >
                             Cancel
                           </button>
                           <button
-                            className="rounded bg-primary px-4 py-2 font-bold text-white hover:bg-bold"
+                            className="px-4 py-2 font-bold text-white rounded bg-primary hover:bg-bold"
                             onClick={handleSubmit}
                           >
                             Compose Letter
@@ -165,4 +203,4 @@ const AddReport = () => {
   );
 };
 
-export default AddReport;
+export default AddLetter;
